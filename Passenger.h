079160b8abfe795a98f486cd2a7f641df28b5a2c
Eric Spoerl <spoeer08@uwgb.edu>
2021-03-25 21:14:34 -0500

    Edited Passenger functions

--- Passenger.h
+++ Passenger.h
@@ -11,25 +11,23 @@ private:
 	string address;
 	string phone;
 	string email;
-	string date;
 	int tickets;
 
 public:
 	Passenger(); //Default Constructor
-	Passenger(string, string, string, string, string, int);
+	Passenger(string, string, string, string, int);
 	void setName(string);
 	void setAddress(string);
 	void setPhone(string);
 	void setEmail(string);
-	void setDate(string);
 	void setTickets(int);
 	string getName() const;
 	string getAddress() const;
 	string getPhone() const;
 	string getEmail() const;
-	string getDate() const;
 	int getTickets() const;
 	void printPassInfo();
+	void makeReservation(Passenger);
 };
 
 #endif
