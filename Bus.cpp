4b8d3c1f576fc883e9958dab5c16dabcfc6343ba
Eric Spoerl <spoeer08@uwgb.edu>
2021-04-02 09:30:03 -0500

    Added more private variables (types of seats, rent, location, etc.)

--- Bus.cpp
+++ Bus.cpp
@@ -4,11 +4,12 @@
 using namespace std;
 
 
-Bus::Bus(int id, string t, bool a, int s) {
+Bus::Bus(int id, string t, bool a, int ts, int ws, int as, string l) {
 	setID(id);
 	setType(t);
 	setAvailability(a);
-	setSeats(s);
+	setSeats(ts, ws, as);
+	setLocation(l);
 }
 
 void Bus::setID(int id) {
@@ -23,8 +24,22 @@ void Bus::setAvailability(bool a) {
 	availability = a;
 }
 
-void Bus::setSeats(int s) {
-	seats = s;
+void Bus::setSeats(int ts, int ws, int as) {
+	totalSeats = ts;
+	winSeats = ws;
+	aisleSeats = as;
+}
+
+void Bus::setLocation(string l) {
+	location = l;
+}
+
+void Bus::setRent(double r) {
+	rent = r;
+}
+
+void Bus::setSecurityDeposit(double s) {
+	securityDeposit = s;
 }
 
 int Bus::getID() const {
@@ -39,11 +54,31 @@ bool Bus::getAvailability() const {
 	return availability;
 }
 
-int Bus::getSeats() const {
-	return seats;
+int Bus::getTotalSeats() const {
+	return totalSeats;
+}
+
+int Bus::getWinSeats() const {
+	return winSeats;
+}
+
+int Bus::getAisleSeats() const {
+	return aisleSeats;
+}
+
+string Bus::getLocation() const {
+	return location;
+}
+
+double Bus::getRent() const {
+	return rent;
+}
+
+double Bus::getSecurityDeposit() const {
+	return securityDeposit;
 }
 
 
 void Bus::printBusInfo() {
-	cout << getID() << "\t" << getType() << "\t" << getAvailability() << "\t" << getSeats() << endl;
+	cout << getID() << "\t" << getType() << "\t" << getAvailability() << "\t" << getTotalSeats() << "\t" << getWinSeats() << "\t" << getAisleSeats()  << endl;
 }
--- Bus.h
+++ Bus.h
@@ -10,18 +10,38 @@ private:
 	int ID;
 	string type;
 	bool availability;
-	int seats;
+	int totalSeats;
+	int winSeats;
+	int aisleSeats;
+	int totalPassengers;
+	double rent;
+	double securityDeposit;
+	double demand;
+	string location;
+
 
 public:
-	Bus(int, string, bool, int);
+	Bus(int, string, bool, int, int, int, string);
 	void setID(int);
 	void setType(string);
 	void setAvailability(bool);
-	void setSeats(int);
+	void setSeats(int, int, int);
+	void setPassengers(int);
+	void setDemand(double);
+	void setLocation(string);
+	void setRent(double);
+	void setSecurityDeposit(double);
 	int getID() const;
 	string getType() const;
 	bool getAvailability() const;
-	int getSeats() const;
+	int getTotalSeats() const;
+	int getWinSeats() const;
+	int getAisleSeats() const;
+	int getPassengers() const;
+	double getDemand() const;
+	string getLocation() const;
+	double getRent() const;
+	double getSecurityDeposit() const;
 	void printBusInfo();
 };
 
